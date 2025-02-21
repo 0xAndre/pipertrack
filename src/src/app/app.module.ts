13f340a649647e2f8913ca04656e5e84667816ee
your_username <ancatos@outlook.com>
2025-02-21 21:37:32 +0000

    setting up the form

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { NgApexchartsModule } from "ng-apexcharts";
 
@@ -46,7 +47,8 @@ import { SettingsComponent } from './settings/settings.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgApexchartsModule
+    NgApexchartsModule,
+    ReactiveFormsModule
   ],
   providers: [IpcService],
   bootstrap: [AppComponent]
--- src/src/app/settings/settings.component.ts
+++ src/src/app/settings/settings.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'pipertrack-settings',
   standalone: false,
-  
+
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  configForm: FormGroup;
 
+  isLoading: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.configForm = this.fb.group({
+      azureUrl: ['', Validators.required],
+      azurePat: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log('Form Values:', this.configForm.value);
+    this.isLoading = true;
+  }
 }
